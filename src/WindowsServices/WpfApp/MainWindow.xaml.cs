785936a4b672e35a4918bb94028ba05af55b8685
Nikolai Znamenskii <Satem1@yandex.ru>
2022-08-08 15:50:52 +0500

    Command buttons as icons.

--- src/WindowsServices/WpfApp/MainWindow.xaml.cs
+++ src/WindowsServices/WpfApp/MainWindow.xaml.cs
@@ -1,5 +1,9 @@
 ﻿namespace WpfApp
 {
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Input;
+    using System.Windows.Media;
     using ViewModels;
 
     /// <summary>
