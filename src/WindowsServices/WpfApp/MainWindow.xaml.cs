705fb2567e1687f76a7caecd42608b3f2ba51a77
Nikolai Znamenskii <Satem1@yandex.ru>
2022-08-08 16:25:42 +0500

    Keyboard controls

--- src/WindowsServices/WpfApp/MainWindow.xaml.cs
+++ src/WindowsServices/WpfApp/MainWindow.xaml.cs
@@ -16,5 +16,79 @@
             DataContext = viewModel;
             InitializeComponent();
         }
+
+        private void WindowsServiceList_OnMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var source = (DependencyObject)e.OriginalSource;
+            while (source != null && source is DataGridRow == false) source = VisualTreeHelper.GetParent(source);
+            if (source == null) return;
+            var row = (DataGridRow)source;
+
+            if (row.IsSelected == false)
+                WindowsServiceList.SelectedItem = ((DataGridRow)source).Item;
+        }
+
+        private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Down:
+                    MoveSelectedElementDown();
+                    break;
+                case Key.Up:
+                    MoveSelectedElementUp();
+                    break;
+                case Key.Right:
+                    MoveFocusRight();
+                    break;
+                case Key.Left:
+                    MoveFocusLeft();
+                    break;
+            }
+        }
+
+        private void MoveSelectedElementDown()
+        {
+            if (WindowsServiceList.Items.Count == 0)
+                return;
+
+            if (WindowsServiceList.SelectedItem == null
+                || WindowsServiceList.SelectedIndex == WindowsServiceList.Items.Count - 1)
+                WindowsServiceList.SelectedItem = WindowsServiceList.Items[0];
+            else
+            {
+                var nextItemIndex = WindowsServiceList.SelectedIndex + 1;
+                WindowsServiceList.SelectedItem = WindowsServiceList.Items[nextItemIndex];
+            }
+
+            WindowsServiceList.ScrollIntoView(WindowsServiceList.SelectedItem);
+        }
+
+        private void MoveSelectedElementUp()
+        {
+            if (WindowsServiceList.Items.Count == 0)
+                return;
+
+            if (WindowsServiceList.SelectedItem == null
+                || WindowsServiceList.SelectedIndex == 0)
+                WindowsServiceList.SelectedItem = WindowsServiceList.Items[WindowsServiceList.Items.Count - 1];
+            else
+            {
+                var previous = WindowsServiceList.SelectedIndex - 1;
+                WindowsServiceList.SelectedItem = WindowsServiceList.Items[previous];
+            }
+
+            WindowsServiceList.ScrollIntoView(WindowsServiceList.SelectedItem);
+        }
+
+        private void MoveFocusRight()
+        {
+            CommandPanel.MoveFocus(new TraversalRequest(FocusNavigationDirection.Right));
+        }
+
+        private void MoveFocusLeft()
+        {
+            CommandPanel.MoveFocus(new TraversalRequest(FocusNavigationDirection.Left));
+        }
     }
 }
